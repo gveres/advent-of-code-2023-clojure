7a689d9484cf8a336339dbbbecd8d0e3b248c9f5
Gabor Veres <gabor.veres@gmail.com>
2023-12-04 10:24:44 +0100

    Day 4 part 2

--- day04.clj
+++ day04.clj
@@ -20,10 +20,12 @@
 
 (defn line->line-data [s]
   (let [;;_ (do (println s) (flush))
+        card-no (read-string (subs s 5 8))
         winning (into #{} (map read-string (filter seq (str/split (subs s 10 39) #" +"))))
         mine (into #{} (map read-string (filter seq (str/split (subs s 42) #" +"))))
         matches (set/intersection winning mine)]
-    {:winning winning
+    {:card-no card-no
+     :winning winning
      :mine mine
      :matches matches
      :num-matches (count matches)
@@ -35,3 +37,36 @@
 (def result-1 (reduce + (map :points all-games)))
 
 result-1;; => 27845
+
+(def one-game (nth all-games 6))
+
+(defn add-won-copies [g]
+  (let [games-to-add
+        (if-not (:claimed g)
+          (range 0 (min (count all-games) (:num-matches g)))
+          '())
+        won-cards (map #(nth all-games (+ % (:card-no g)) nil) games-to-add)]
+    (concat [(assoc g :claimed true)] (filter some? won-cards))))
+
+(map :card-no (add-won-copies one-game))
+
+(count (mapcat add-won-copies all-games))
+
+(count (remove :claimed all-games))
+
+
+(defn recursive-claim [cards]
+  (loop [cards cards]
+    (let [_ (do (println "Now I have " (count cards) "cards") (flush))
+          unclaimed (count (remove :claimed cards))]
+      (if (= 0 unclaimed)
+        (count cards)
+
+        (recur (mapcat add-won-copies cards))))))
+
+(comment
+  (recursive-claim all-games))
+
+(def result-2 (recursive-claim all-games))
+
+;;result-2 is 9496801
