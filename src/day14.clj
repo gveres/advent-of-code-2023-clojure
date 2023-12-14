8efb34b93153c7ecf9e494ffb57fbfd766f61e40
Gabor Veres <gabor.veres@gmail.com>
2023-12-14 18:32:58 +0100

    Day 14 part 2

--- src/day14.clj
+++ src/day14.clj
@@ -25,10 +25,10 @@
    "#OO..#...."
    ])
 
-(defn transpose-lines [lines]
+(defn transpose-lines* [lines]
   (map (partial apply str) (utils/transpose lines)))
 
-
+(def transpose-lines (memoize transpose-lines*))
 
 (defn tilt-left [line]
   (->> line
@@ -36,8 +36,23 @@
        (mapcat (comp reverse sort))
        (apply str)))
 
+(defn tilt-right [line]
+  (->> line
+       (partition-by #(= % \#))
+       (mapcat sort)
+       (apply str)))
+
 (defn tilt-north [lines]
   (transpose-lines (map tilt-left (transpose-lines lines))))
+
+(defn tilt-south [lines]
+  (transpose-lines (map tilt-right (transpose-lines lines))))
+
+(defn tilt-west [lines]
+  (map tilt-left lines))
+
+(defn tilt-east [lines]
+  (map tilt-right lines))
 
 ;; The amount of load caused by a single rounded rock (O) is equal to the number of rows from the rock to the south edge of the platform, including the row the rock is on. (Cube-shaped rocks (#) don't contribute to load.) So, the amount of load caused by each rock in each row is as follows:
 
@@ -55,3 +70,49 @@
   (reduce + (-> lines
                 tilt-north
                 calculate-line-loads-north)))
+
+;;part 2
+
+(defn spin-cycle* [lines]
+  (-> lines
+      tilt-north
+      tilt-west
+      tilt-south
+      tilt-east))
+
+(def spin-cycle (memoize spin-cycle*))
+
+(def spinned-seq (iterate spin-cycle lines))
+
+(defn lead-in-and-cycle-size [seq]
+  (loop [knowns #{}
+         knowns-to-index {}
+         index 0]
+    (let [current (nth seq index)]
+      (if (knowns current)
+        [(get knowns-to-index current) (- index (get knowns-to-index current))]
+        (recur (conj knowns current)
+               (assoc knowns-to-index current index)
+               (inc index))))))
+
+(lead-in-and-cycle-size spinned-seq)
+;; lead-in is 124 , cycle size is 26 for the real one
+
+(lead-in-and-cycle-size (iterate spin-cycle sample-lines))
+;; 3 and 7 for the sample
+
+(defn shrink-cycle [n lead-in cycle-size]
+  (+ lead-in (rem (- n lead-in) cycle-size)))
+
+(defn calculate-result-2 [input]
+  (let [spin-seq (iterate spin-cycle input)
+        [lead-in cycle-size] (lead-in-and-cycle-size spin-seq)
+        result-index (shrink-cycle 1000000000 lead-in cycle-size)]
+    (reduce + (calculate-line-loads-north (nth spin-seq result-index)))))
+
+
+(calculate-result-2 sample-lines)
+;; => 64
+
+(time (calculate-result-2 lines))
+;; => 105008
